35124aa64c1036eb206fd5646384b670d9748c4d
Michel Klopfenstein <Michel.Klopfenstein@theglobalfund.org>
2026-09-14 13:38:29 +0200

    Publier le sitemap SEO

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -3,7 +3,7 @@ import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
 
 export const onRequest: PagesFunction = async (context) => {
   // @ts-expect-error - the Remix server build is generated during the build step and is not present during typecheck.
-  const serverBuild = await import('../build/server/index.js');
+  const serverBuild = await import('../build/server');
 
   const handler = createPagesFunctionHandler({
     build: serverBuild as any,
